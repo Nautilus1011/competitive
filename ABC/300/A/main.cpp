f061dc6eda44105f7fb1184f09622071b2ecaf6c
Nautilus1011 <jin.ysd@hotmail.com>
2025-06-02 23:46:37 +0900

    create development environment

---
+++ ABC/300/A/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // 入出力を行うために必要なヘッダーファイルを読み込む
+
+int main() {        // プログラムが最初に実行される場所（メイン関数）
+    std::cout << "Hello, World!" << std::endl; // "Hello, World!" を画面に出力し、改行する
+    return 0;       // プログラムが正常終了したことをOSに伝える
+}
